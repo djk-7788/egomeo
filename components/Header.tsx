ed9b5d7e2172aac4e71e5fa433de3b4ed829a15b
djk-7788 <jeer7788@gmail.com>
2026-06-04 13:00:59 +0900

    style: 헤더 로고 이미지 교체 (이게_머고.png → 2.png)

--- components/Header.tsx
+++ components/Header.tsx
@@ -45,7 +45,7 @@ export default function Header() {
       <div className="max-w-screen-xl mx-auto pl-0 pr-3 sm:pl-0 sm:pr-4 h-[74px] flex items-center justify-between">
         <Link href="/">
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/이게_머고.png" alt="이게머고?" style={{ height: "66px", width: "auto" }} />
+          <img src="/2.png" alt="이게머고?" style={{ height: "66px", width: "auto" }} />
         </Link>
         <div className="flex items-center gap-4 sm:gap-5">
           <button
